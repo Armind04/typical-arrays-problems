7ab68ff4900de1ef326443803b9fb05d89eaf901
Armind04 <57454858+Armind04@users.noreply.github.com>
2021-03-09 23:57:13 +0200

    feat: complete functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+	return array.length == 0 ? 0 : Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+ 	return array.length == 0 ? 0 : Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array  = []) {
+	if (array.length == 0) return 0;
+	return array.reduce((sum, x) => {
+   		return sum + x;
+ 	}, 0) / [...array].length
 }
